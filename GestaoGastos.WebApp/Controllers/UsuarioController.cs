e20f48b7365d91d2b2f1b80fa9e0334a1dabfa95
Rafael <rafaelnd2001@gmail.com>
2025-11-24 16:48:57 -0300

    Add project files.

---
+++ GestaoGastos.WebApp/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoGastos.WebApp.Controllers
+{
+   public class UsuarioController : Controller
+   {
+      public IActionResult Login()
+      {
+         return View();
+      }
+
+      public IActionResult Registrar()
+      {
+         return View();
+      }
+   }
+}
